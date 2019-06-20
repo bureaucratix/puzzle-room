5f8c48c60d2b345c4dbb307e2aa8753365963c39
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-20 00:06:34 -0700

    dragg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
+    drag();
 });
 
 function clearScene(){
@@ -7,4 +8,26 @@ function clearScene(){
     while (container.firstChild) {
     container.removeChild(container.firstChild);
     }
+}
+
+
+function landingPage(){
+    const chessboard = document.querySelector('.chessboard')
+    chessboard.remove();
+    const container = document.getElementById('content')
+    document.body.style.background = 'blue'
+    let h1 = document.createElement('h1')
+    
+    h1.textContent = 'Enter If You Dare'
+    container.appendChild(h1)
+    let form = document.createElement('form')
+    let input = document.createElement('input')
+    let submit = document.createElement('input')
+    submit.setAttribute('type', 'submit')
+
+    container.appendChild(form)
+    form.appendChild(input)
+    form.appendChild(submit)
+
+    
 }
