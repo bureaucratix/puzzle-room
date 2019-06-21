0f3df30340113c23f426ee5bd19014afd610d2ae
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-20 17:38:12 -0700

    d

--- frontend/src/coffee.js
+++ frontend/src/coffee.js
@@ -1,6 +1,21 @@
 function loadCoffeePage(){
     clearScene();
-document.body.style.backgroundImage = "url('../frontend/images/coffee.jpg')";
-showLetter('E')
+    document.body.style.backgroundImage = "url('../frontend/images/coffee.jpg')";
+    showLetter('E');
+    activeUser.found_complete = true 
+    updateUser(activeUser)
+
+    
+
+let backButton = document.createElement('img')
+    let container = document.getElementById('content')
+    backButton.src = "./images/left.png";
+    container.appendChild(backButton);
+    backButton.style= "float: left; height: 100px; width: 100px;"
+    backButton.addEventListener('click', ()=>{
+        loadKitchen();
+    })
+
+    
 }
 
